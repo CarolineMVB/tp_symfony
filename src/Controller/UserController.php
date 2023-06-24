fec9e9fa492c36da1b30ce8a7595e5c8b633d68c
CarolineMVB <caroline.beauchet@yahoo.fr>
2023-06-24 10:39:44 +0200

    make:admin:dashboard

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class UserController extends AbstractController
 {
-    #[Route('/user/{id}', name: 'user_show')]
+    #[Route('/user', name: 'app_user')]
     public function index(): Response
     {
         return $this->render('user/index.html.twig', [
